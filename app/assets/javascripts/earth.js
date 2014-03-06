2dfc4b1eb5d326085a9f6270d9be68ac7e606c8f
programthis <kylebachan@gmail.com>
2014-03-06 13:30:16 -0500

    Added re-centre button

--- app/assets/javascripts/earth.js
+++ app/assets/javascripts/earth.js
@@ -28,44 +28,11 @@ $(document).ready(function(){
       addSampleButton('Homeward Bound', showEarth);
       addSampleButton('Show Sun (Dusk/Dawn)', showSun);
       addSampleButton('Hide Sun', hideSun);
+      addSampleButton('Re-Centre', reCentre);
 
       var countries = gon.countries;
       var cities = gon.cities;
-      // var options = [];
-
-      //setting the query for all the countries in the world
-    //   function setCountryOption(country, index){
-    //   	options[index] = { "format" : "300x250", 
-    //   "queryList" : [
-    //         {
-    //           "title" : "World News",
-    //           "q" : countries[index].name
-    //         },
-    //         {
-    //           "q" : countries[index].name
-    //         }
-    //       ]
-  		// };
-    //   }	
-    //   countries.forEach(setCountryOption);
-
-    //   //setting the query for all the countries in the world
-    //   function setCityOption(city, index){
-    //   	options[index] = { "format" : "300x250", 
-    //   "queryList" : [
-    //         {
-    //           "title" : "World News",
-    //           "q" : cities[index].name
-    //         },
-    //         {
-    //           "q" : cities[index].name
-    //         }
-    //       ]
-  		// };
-    //   }	
-    //   cities.forEach(setCityOption);
-
-    //   var content = document.getElementById("content");
+      Search();
     }
 
     function initCB(instance) {
@@ -101,7 +68,7 @@ $(document).ready(function(){
       var cities = gon.cities;
       
       countries.forEach(createPlacemarkForCountry);
-      cities.forEach(createPlacemarkForCity);
+      // cities.forEach(createPlacemarkForCity);
     }
 
     function createPlacemarkForCountry(country){
@@ -114,17 +81,7 @@ $(document).ready(function(){
     	var placemark = ge.createPlacemark('');
     	placemark.setName(country.name);
 	    ge.getFeatures().appendChild(placemark);
-	  
-	    // Create style map for placemark
-	    // var icon = ge.createIcon('');
-	    // icon.setHref('http://maps.google.com/mapfiles/kml/shapes/triangle.png');
-	    // var style = ge.createStyle('');
-	    // style.getIconStyle().setIcon(icon);
-	    // style.getIconStyle().setScale(5.0);
-
-	    // placemark.setStyleSelector(style);
-	  
-
+	  	  
 	    // Create a style map.
 	    var styleMap = ge.createStyleMap('');
 
@@ -147,7 +104,6 @@ $(document).ready(function(){
 
 	    // Apply stylemap to a placemark.
 	    placemark.setStyleSelector(styleMap);
-
 
 
 	    // Create point
@@ -241,13 +197,155 @@ $(document).ready(function(){
 				};
 			var content = document.getElementById("content");
 			var newsShow = new google.elements.NewsShow(content,options);
-
 	    });
 
     }
 
     function failureCB(errorCode) {
     }
+
+   var AppId = "7DkdEuUKwIAzix/CqNuIqXdJ1joqegBN+BmPUQ3NHZU";
+   
+   function Search()
+   {
+       var requestStr = "http://api.bing.net/json.aspx?"
+       
+           // Common request fields (required)
+           + "AppId=" + AppId
+           + "&Query=Kyle"
+           + "&Sources=News"
+           
+           // Common request fields (optional)
+           + "&Version=2.0"
+           + "&Market=en-us"
+           + "&Options=EnableHighlighting"
+
+           // News-specific request fields (optional)
+           + "&News.Offset=0"
+           
+           // The following request fields are mutually exclusive.
+           // Uncomment the line corresponding to the request field you wish
+           // to use.
+           ////+ "&News.LocationOverride=US.WA"
+           ////+ "&News.Category=rt_Political"
+           + "&News.SortBy=Relevance"
+           
+           // JSON-specific request fields (optional)
+           + "&JsonType=callback"
+           + "&JsonCallback=SearchCompleted";
+
+          console.log(requestStr);
+        var requestScript = document.getElementById("searchCallback");
+        requestScript.src = requestStr;
+
+   }
+
+   function SearchCompleted(response)
+   {
+   	   alert("I was called");
+       var errors = response.SearchResponse.Errors;
+       if (errors != null)
+       {
+           // There are errors in the response. Display error details.
+           DisplayErrors(errors);
+       }
+       else
+       {
+           // There were no errors in the response. Display the
+           // News results.
+           DisplayResults(response);
+       }
+   }
+
+   function DisplayResults(response)
+   {
+       var output = document.getElementById("newsfeed");
+       var resultsHeader = document.createElement("h4");
+       var resultsList = document.createElement("ul");
+       output.appendChild(resultsHeader);
+       output.appendChild(resultsList);
+   
+       var results = response.SearchResponse.News.Results;
+       
+       // Display the results header.
+       resultsHeader.innerHTML = "Bing API Version "
+           + response.SearchResponse.Version
+           + "<br />News results for "
+           + response.SearchResponse.Query.SearchTerms
+           + "<br />Displaying "
+           + (response.SearchResponse.News.Offset + 1)
+           + " to "
+           + (response.SearchResponse.News.Offset + results.length)
+           + " of "
+           + response.SearchResponse.News.Total
+           + " results<br />";
+       
+       // Display the News results.
+       var resultsListItem = null;
+       var resultStr = "";
+       for (var i = 0; i < results.length; ++i)
+       {
+           resultsListItem = document.createElement("li");
+           resultsList.appendChild(resultsListItem);
+           resultStr = "<a href=\""
+               + results[i].Url
+               + "\">"
+               + results[i].Title
+               + "</a><br />"
+               + results[i].Source
+               + "<br />"
+               + results[i].Date
+               + "<br />"
+               + results[i].Snippet
+               + "<br /><br />";
+           console.log(resultStr);
+           // Replace highlighting characters with strong tags.
+           resultsListItem.innerHTML = ReplaceHighlightingCharacters(
+               resultStr,
+               "<strong>",
+               "</strong>");
+       }
+   }
+   
+   function ReplaceHighlightingCharacters(text, beginStr, endStr)
+   {
+       // Replace all occurrences of U+E000 (begin highlighting) with
+       // beginStr. Replace all occurrences of U+E001 (end highlighting)
+       // with endStr.
+       var regexBegin = new RegExp("\uE000", "g");
+       var regexEnd = new RegExp("\uE001", "g");
+             
+       return text.replace(regexBegin, beginStr).replace(regexEnd, endStr);
+   }
+
+   function DisplayErrors(errors)
+   {
+       var output = document.getElementById("newsfeed");
+       var errorsHeader = document.createElement("h4");
+       var errorsList = document.createElement("ul");
+       output.appendChild(errorsHeader);
+       output.appendChild(errorsList);
+       
+       // Iterate over the list of errors and display error details.
+       errorsHeader.innerHTML = "Errors:";
+       var errorsListItem = null;
+       for (var i = 0; i < errors.length; ++i)
+       {
+           errorsListItem = document.createElement("li");
+           errorsList.appendChild(errorsListItem);
+           errorsListItem.innerHTML = "";
+           for (var errorDetail in errors[i])
+           {
+               errorsListItem.innerHTML += errorDetail
+                   + ": "
+                   + errors[i][errorDetail]
+                   + "<br />";
+           }
+           
+           errorsListItem.innerHTML += "<br />";
+       }
+   }
+
 
     function showSky() {
     	// create the placemark
@@ -311,6 +409,18 @@ $(document).ready(function(){
 
     function hideSun() {
       ge.getSun().setVisibility(false);
+    }
+    function reCentre() {
+      //making it so the earth look at the "non-western" side of the world upon load
+      var la = ge.createLookAt('');
+      la.set(48, 31,
+        0, // altitude
+        ge.ALTITUDE_RELATIVE_TO_GROUND,
+        0, // heading
+        0, // straight-down tilt
+        8000000 // range (inverse of zoom)
+        );
+      ge.getView().setAbstractView(la);
     }
 
     google.setOnLoadCallback(init);
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,14 @@ class WelcomeController < ApplicationController
 	@regions = Region.all
 
 	gon.countries = @countries
+
+	# using Bing API to get news based on country
+	# gon.bing = RBing.new("7DkdEuUKwIAzix/CqNuIqXdJ1joqegBN+BmPUQ3NHZU")
+
+	# rsp = bing.web("Jenna Quint")
+	# puts "*********************************"
+	# puts rsp.results[0].title
+
 	# gon.cities = @cities
 	# gon.regions = @regions
   end

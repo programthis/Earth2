57c8dc8bc53ac1da913ae645d6ee7d7fe706cb7f
bmackin <mackin.beth@gmail.com>
2014-03-07 16:00:50 -0500

    updating news feed

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,15 @@
+
+  //   $("#toggleBtn").click(function(){
+  //   //  $(".menu").toggle("slide");
+
+  //   // });
+  //   });
+
+
+  // $(document).on('page:change',function() {
+  // 	$("#toggleBtn").click(function() {
+  // 		$(".menu").animate({
+  // 			left: '-=' + $(this).width
+  // 		}, 2000);
+  // 	})
+  // })

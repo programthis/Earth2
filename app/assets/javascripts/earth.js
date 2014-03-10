877bd45115f7e5fcfd4b8130546f1cdcbe9198bf
bmackin <mackin.beth@gmail.com>
2014-03-10 16:42:29 -0400

    styles

--- app/assets/javascripts/earth.js
+++ app/assets/javascripts/earth.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
-  // var news = gon.news;
+  var news = gon.news;
 
-  // var titles = [];
-  // var urls = [];
+  var titles = [];
+  var urls = [];
 
-  // for (var i =0; i < news.articles.length; i++){
-  //     titles[i] = news.articles[i].title;
-  //     urls[i] = news.articles[i].url;
-  // }
+  for (var i =0; i < news.articles.length; i++){
+      titles[i] = news.articles[i].title;
+      urls[i] = news.articles[i].url;
+  }
 
   //searching for breaking news in an interval of 3 minutes
   var interval = 0;
@@ -95,19 +95,19 @@ $(document).ready(function(){
   //   });
   // },3000);
   
-  // function urlFunction(){
-  //   var a;
-  //   var node;
-  //   for (var i =0; i < titles.length; i++){
-  //     a = document.createElement("a");
-  //     node=document.createTextNode(titles[i]);
-  //     a.setAttribute("href", urls[i]);
-  //     a.setAttribute("target", "blank");
-  //     a.innerHTML = titles[i] + "    ";
-  //     document.getElementById('ticker').appendChild(a);
-  //   }
-  // }
-  // setTimeout(urlFunction, 3000);
+  function urlFunction(){
+    var a;
+    var node;
+    for (var i =0; i < titles.length; i++){
+      a = document.createElement("a");
+      node=document.createTextNode(titles[i]);
+      a.setAttribute("href", urls[i]);
+      a.setAttribute("target", "blank");
+      a.innerHTML = titles[i] + "    ";
+      document.getElementById('ticker').appendChild(a);
+    }
+  }
+  setTimeout(urlFunction, 180000);
 
 
   var ge;

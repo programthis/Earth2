cec577a2b7059504d6db289bed66b3881e07946f
programthis <kylebachan@gmail.com>
2014-03-12 23:57:23 -0400

    Got newsfeed fully populating based on user subscriptions

--- app/assets/javascripts/earth.js
+++ app/assets/javascripts/earth.js
@@ -15,62 +15,12 @@ $(document).ready(function(){
     var countryName = countries[randomCountry].name;
     var breakingNewsLat = countries[randomCountry].latitude;
     var breakingNewsLong = countries[randomCountry].longitude;
+
     console.log("TEST STARTS HERE");
     console.log(randomCountry);
     console.log(breakingNewsLat);
     console.log(breakingNewsLong);
 
-    $.bingSearch({
-        query: countryName,
-        latitude: breakingNewsLat,
-        longitude: breakingNewsLong,
-        appKey: '7DkdEuUKwIAzix/CqNuIqXdJ1joqegBN+BmPUQ3NHZU',
-        // Optidefaults to the Bing Search API Web Results Query).
-        // Additional information: This feature allows you to proxy through a server-side
-        //                         script in order to hide your API key, which is exposed to the
-        //                         world if you set it client-side in appKey. An example PHP
-        //                         script is included (searchproxy.php).
-        // Optional (defaults to 1): Page Number
-        pageNumber: 1,
-        urlBase: 'https://api.datamarket.azure.com/Bing/Search/v1/News',
-        // Optional (defaults to 10): Page Size
-        pageSize: 1,
-        // Optional: Function is called after search results are retrieved, but before the interator is called
-        beforeSearchResults: function(data) {
-            // Use data.hasMore, data.resultBatchCount
-        },
-        // Optional: Function is called once per result in the current batch
-        searchResultIterator: function(data) {
-            // Use data.ID, data.Title, data.Description, data.Url, data.DisplayUrl, data.Metadata.Type (check for undefined)
-            console.log(data.Title);
-            console.log(data.Description);
-            console.log(data.Url);
-            var news = document.createElement("div");
-            news.setAttribute("class", "article");
-
-            // //creating link and title for article
-            // var newsItem = document.createElement("a");
-            // newsItem.setAttribute("href", data.Url);
-            // newsItem.setAttribute("target", "blank");
-            // newsItem.innerHTML = data.Title
-
-            // //creating description for article
-            // var newsDescription = document.createElement("p");
-            // newsDescription.innerHTML = data.Description;
-            // news.appendChild(newsItem);
-            // news.appendChild(newsDescription);
-
-        },
-        // Optional: Function is called after search results are retrieved and after all instances of the interator are called
-        afterSearchResults: function(data) {
-            // Use data.hasMore, data.resultBatchCount
-        },
-        // Optional: Called when there is an error retrieving results
-        fail: function(data) {
-            // data contains an error message
-            console.log('bing search fail!');
-        }
-    });
     createPlacemarkForBreakingNews(countryName, breakingNewsLat,breakingNewsLong);
   },1800000000000);
 
@@ -238,7 +188,6 @@ $(document).ready(function(){
       // var terrainLayer = layerRoot.getLayerById(ge.LAYER_TERRAIN);
       // terrainLayer.setVisibility(true);
 
-
       //making it so the earth look at the "non-western" side of the world upon load
       var la = ge.createLookAt('');
       la.set(48, 31,
@@ -253,7 +202,6 @@ $(document).ready(function(){
       countries.forEach(createPlacemarkForCountry);
       capitals.forEach(createPlacemarkForCapital);
       
-
     }
 
     function createPlacemarkForCountry(country){
---
+++ app/assets/javascripts/subscription.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+	var interval;
+	var countries = gon.newsfeed;
+
+	interval = setInterval(function(){
+		var numCountries = countries.length;
+		console.log(numCountries);
+		var randomCountry = Math.floor((Math.random()*numCountries)+0);
+		var newsfeedCountry = countries[randomCountry].name;
+		var newsfeedLatitude = countries[randomCountry].latitude;
+		var newsfeedLongitude = countries[randomCountry].longitude;
+
+		console.log(newsfeedCountry);
+
+		populateNewsfeed(newsfeedCountry,newsfeedLatitude, newsfeedLongitude);
+	},500000);
+
+	function populateNewsfeed(newsfeedCountry,newsfeedLatitude,newsfeedLongitude){
+
+		$.bingSearch({
+		    query: newsfeedCountry,
+		    latitude: newsfeedLatitude,
+		    longitude: newsfeedLongitude,
+
+		    appKey: '7DkdEuUKwIAzix/CqNuIqXdJ1joqegBN+BmPUQ3NHZU',
+		    // Optidefaults to the Bing Search API Web Results Query).
+		    // Additional information: This feature allows you to proxy through a server-side
+		    //                         script in order to hide your API key, which is exposed to the
+		    //                         world if you set it client-side in appKey. An example PHP
+		    //                         script is included (searchproxy.php).
+		    // Optional (defaults to 1): Page Number
+		    pageNumber: 1,
+		    urlBase: 'https://api.datamarket.azure.com/Bing/Search/v1/News',
+		    // Optional (defaults to 10): Page Size
+		    pageSize: 1,
+		    // Optional: Function is called after search results are retrieved, but before the interator is called
+		    beforeSearchResults: function(data) {
+		        // Use data.hasMore, data.resultBatchCount
+		    },
+		    // Optional: Function is called once per result in the current batch
+		    searchResultIterator: function(data) {
+		        // Use data.ID, data.Title, data.Description, data.Url, data.DisplayUrl, data.Metadata.Type (check for undefined)
+		        console.log(data.Title);
+		        console.log(data.Description);
+		        console.log(data.Url);
+		        var news = document.createElement("div");
+		        news.setAttribute("class", "article");
+
+		        //creating link and title for article
+		        var newsItem = document.createElement("a");
+		        newsItem.setAttribute("href", data.Url);
+		        newsItem.setAttribute("target", "blank");
+		        newsItem.innerHTML = data.Title
+		        news.appendChild(newsItem);
+
+		        $("#newsfeed").prepend(news);
+
+		    },
+		    // Optional: Function is called after search results are retrieved and after all instances of the interator are called
+		    afterSearchResults: function(data) {
+		        // Use data.hasMore, data.resultBatchCount
+		    },
+		    // Optional: Called when there is an error retrieving results
+		    fail: function(data) {
+		        // data contains an error message
+		        console.log('bing search fail!');
+		    }
+		}); 
+
+	}
+
+});

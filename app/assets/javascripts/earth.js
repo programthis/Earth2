a92870f0f58a2f27e822eee0b2f92ccf7636751c
programthis <kylebachan@gmail.com>
2014-03-14 15:58:39 -0400

    Added all of Beth's navbar highlighting changes to the master

--- app/assets/javascripts/earth.js
+++ app/assets/javascripts/earth.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
     var breakingNewsLong = countries[randomCountry].longitude;
     createPlacemarkForBreakingNews(countryName, breakingNewsLat,breakingNewsLong);
     
-  },1800000000000);
+  },180000);
 
     function createPlacemarkForBreakingNews(breakingNewsCountry, breakingNewsLat, breakingNewsLong){
         

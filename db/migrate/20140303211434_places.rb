af5af64dd9892dcc887e2ef9cd2dae38be3f1640
programthis <kylebachan@gmail.com>
2014-03-17 01:28:53 -0400

    Updated migration file

--- db/migrate/20140303211434_places.rb
+++ db/migrate/20140303211434_places.rb
@@ -54,7 +54,7 @@ class Places < ActiveRecord::Migration
     #change encoding to the one used into files
     execute "set client_encoding to 'latin1'"
     execute "COPY countries FROM STDIN DELIMITER ',' CSV HEADER;"
-    execute "COPY regions FROM STDIN DELIMITER ',' CSV HEADER;"
+    # execute "COPY regions FROM STDIN DELIMITER ',' CSV HEADER;"
     execute "COPY cities FROM STDIN DELIMITER ',' CSV HEADER;"
     #change encoding back to UTF8
     execute "set client_encoding to 'UTF8'"

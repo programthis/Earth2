137bb5e613969ee9f696d452db910954de36b168
programthis <kylebachan@gmail.com>
2015-08-02 15:46:39 -0400

    Updated formatting

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,9 +5,8 @@ class WelcomeController < ApplicationController
 	@user = User.new
 	gon.newsfeed_path = url_for(newsfeeds_path)
 	
- 	top_articles_data = open('http://content.guardianapis.com/search?api-key=' + ENV["GUARDIAN_KEY"] + "&section=-football,-sport").read
+ 	top_articles_data = open("http://content.guardianapis.com/search?api-key=" + ENV["GUARDIAN_KEY"] + "&section=-football,-sport").read
  	top_articles_json = JSON.parse(top_articles_data)
  	gon.news = top_articles_json["response"]["results"]
   end
-	
 end

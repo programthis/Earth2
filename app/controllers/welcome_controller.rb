202e0261c14f66f5c99b3f0b546885a04da9c6a4
programthis <kylebachan@gmail.com>
2015-08-16 16:37:02 -0400

    Filtered tv and radio out of the ticker news feed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,7 @@ class WelcomeController < ApplicationController
 	@user = User.new
 	gon.newsfeed_path = url_for(newsfeeds_path)
 	
- 	top_articles_data = open("http://content.guardianapis.com/search?api-key=" + ENV["GUARDIAN_KEY"] + "&section=-football,-sport").read
+ 	top_articles_data = open("http://content.guardianapis.com/search?api-key=" + ENV["GUARDIAN_KEY"] + "&section=-football,-sport,-tv-and-radio").read
  	top_articles_json = JSON.parse(top_articles_data)
  	gon.news = top_articles_json["response"]["results"]
   end
